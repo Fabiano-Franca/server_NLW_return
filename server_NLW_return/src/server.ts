ec0a0cf3f240d8292270d4d34d914849fa660e3d
Fabiano-Franca <fabianofranca.ti@gmail.com>
2022-05-07 22:40:37 -0300

    Aula 03 - Envio de email

--- server_NLW_return/src/server.ts
+++ server_NLW_return/src/server.ts
@@ -1,9 +1,45 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
+import { prisma } from './prisma';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    return res.send('Hello World');
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "d1b4c279da858c",
+      pass: "071ea3829c5c91"
+    }
+  });
+
+app.get('/feedbacks', async (req, res) => {
+    const { type, comment, screenshot } = req.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type: type,
+            comment: comment,
+            screenshot: screenshot
+        }
+    });
+
+    await transport.sendMail({
+        from: 'Equipe Feedget <oi.feedget.com>',
+        to: 'Fabiano França <fabianofranca.ti@gmail.com>',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`,
+            `</div>`,
+
+        ].join('\n')
+    });
+
+    return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
